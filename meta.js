9256ac23e3f553ff89e53f5219d3746e187e63f4
areyouse7en <373084212@qq.com>
2017-11-26 21:12:12 +0800

    router

--- meta.js
+++ meta.js
@@ -40,8 +40,5 @@ module.exports = {
       ]
     }
   },
-  "filters": {
-    "src/router/**/*": "router"
-  },
   "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
 };
